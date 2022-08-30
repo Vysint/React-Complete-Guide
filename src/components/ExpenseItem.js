e32ed21dd7d042181aaa4e79b69ffaaed36a8c0d
Vincent Kipngetich <vinniebluz@icloud.com>
2022-08-30 12:09:12 +0300

    Outputted Dynamic Data in the custom component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItem.css';
 
 const ExpenseItem = () => {
+    const expenseDate = new Date(2022, 8, 23);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67
   return (
     <div className="expense-item">
-      <div>April 28th 2022</div>
+      <div>{expenseDate.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
