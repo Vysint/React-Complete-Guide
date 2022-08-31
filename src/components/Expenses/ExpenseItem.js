37271b6f503d9628a08c69aaadd4169f852f80b7
Vincent Kipngetich <vinniebluz@icloud.com>
2022-08-31 11:57:55 +0300

    Added State Management - useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
-const ExpenseItem = ({ title, amount, date }) => {
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Updated');
+  }
   return (
     <Card className="expense-item">
-      <ExpenseDate  date = {date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick = {clickHandler}>Change Title</button>
     </Card>
   );
 };
 
 export default ExpenseItem;
+
