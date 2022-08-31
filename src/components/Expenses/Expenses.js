45032247f54c2fd9350b09ea7626f25c500bf7e4
Vincent Kipngetich <vinniebluz@icloud.com>
2022-08-31 21:35:24 +0300

    Rendered List of data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,26 +16,10 @@ const Expenses = ({ items }) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      <ExpenseItem
-        title={items[0].title}
-        amount={items[0].amount}
-        date={items[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={items[1].title}
-        amount={items[1].amount}
-        date={items[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={items[2].title}
-        amount={items[2].amount}
-        date={items[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={items[3].title}
-        amount={items[3].amount}
-        date={items[3].date}
-      ></ExpenseItem>
+
+      {items.map((item) => (
+        <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+      ))}
     </Card>
   );
 };
