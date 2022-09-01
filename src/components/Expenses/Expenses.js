9d8fadbceb36550075c4274955ac7b8de93a85da
Vincent Kipngetich <vinniebluz@icloud.com>
2022-09-01 20:23:28 +0300

    Added filtered Data by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,12 +5,16 @@ import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
-const Expenses = (props) => {
+const Expenses = ({items}) => {
   const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  })
 
   return (
     <div>
@@ -19,7 +23,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
+        {filteredExpenses.map((expense) => (
           <ExpenseItem
             key = {expense.id}
             title={expense.title}
